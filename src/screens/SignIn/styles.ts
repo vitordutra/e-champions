10f13c664b7b1ec459354a9420c9c8099a836ba7
Vitor Dutra <jo.vitor.d@gmail.com>
2021-08-17 21:50:50 -0300

    Finish Login and Home Screen

--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,4 +1,5 @@
 import { StyleSheet } from 'react-native';
+
 import { theme } from '../../global/styles/theme';
 
 export const styles = StyleSheet.create({
@@ -6,29 +7,29 @@ export const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: theme.colors.background,
   },
-
   image: {
     width: '100%',
     height: 360,
   },
-
   content: {
     marginTop: -40,
+    paddingHorizontal: 50,
   },
-
   title: {
     color: theme.colors.heading,
     textAlign: 'center',
     fontSize: 40,
     marginBottom: 16,
+    fontFamily: theme.fonts.title700,
+    lineHeight: 40,
   },
-  
   subtitle: {
     color: theme.colors.heading,
     fontSize: 15,
     textAlign: 'center',
-    marginBottom: 64
-  }
-})
+    marginBottom: 64,
+    fontFamily: theme.fonts.title500,
+    lineHeight: 25,
+  },
+});
